4ff836b1295c62018849cdeca196b60b11aab733
priory <dev@priory.dev>
2024-07-25 23:05:13 +0200

    feat: wip part rotation

--- ArenaOrder.lua
+++ ArenaOrder.lua
@@ -1,16 +1,73 @@
+if (table.indexOf == nil) then
+    table.indexOf = function(t, object)
+        if type(t) ~= "table" then error("table expected, got " .. type(t), 2) end
+
+        for i, v in pairs(t) do
+            if object == v then
+                return i
+            end
+        end
+    end
+end
+
+if (table.exchange == nil) then
+    table.exchange = function(t, a, b)
+        if t[a] == nil or t[b] == nil then error("array index out of range") end
+
+        local c = t[a]
+        t[a] = t[b]
+        t[b] = c
+    end
+end
+
+
 local PARTY_SIZE = 5
-local ADDON_NAME = "ArenaOrder"
+local ADDON_NAME, L = ...
 local ADDON_MACRO_COMMAND = ADDON_NAME
 local ADDON_COMMAND = "ao"
+local PLAYER_NAME = UnitName("player");
 
-local DEBUG = false
+local DEBUG = true
 
 if (DEBUG) then
-    local DEBUG_UNIT_NAME = "Beta"
-    local DEBUG_PARTY_NAMES = { "Alpha", "Beta", "Charlie", "Delta" }
+    DEBUG_UNIT_NAME = "Beta"
+    DEBUG_PARTY_NAMES = { "Alpha", "Beta", "Charlie", "Delta" }
+
+    DEBUG_ARENA_NAME = "Two"
+    DEBUG_ARENA_NAMES = { "One", "Two", "Three", "Four", "Five" }
 end
 
 SLASH_ARENA_ORDER1 = "/" .. ADDON_COMMAND
+
+local frame = CreateFrame("FRAME")
+frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("PLAYER_LOGOUT")
+
+function frame:OnEvent(event, arg1)
+    if event == "ADDON_LOADED" and arg1 == ADDON_NAME then
+        if AO == nil then
+            AO = {}
+            AO["party"] = {}
+            AO["arena"] = {}
+
+            for i = 1, PARTY_SIZE - 1, 1 do
+                AO["party"][i] = i;
+            end
+
+            for i = 1, PARTY_SIZE, 1 do
+                AO["arena"][i] = i;
+            end
+        end
+
+        if AO then
+            -- code
+        end
+    elseif event == "PLAYER_LOGOUT" then
+        -- code
+    end
+end
+
+frame:SetScript("OnEvent", frame.OnEvent);
 
 --[[
 TODO:
@@ -18,7 +75,7 @@ TODO:
 [ ] Create localization strings.
 ]] --
 
-local function ProcessLine(line)
+local function ProcessLine(line, unitN)
     if (string.len(line) == 0) then
         return 1, nil
     end
@@ -27,7 +84,7 @@ local function ProcessLine(line)
         return 2, nil
     end
 
-    local s, n = string.gsub(line, "^#" .. ADDON_MACRO_COMMAND .. " (party[1-9][0-9]*)$", "%1")
+    local s, n = string.gsub(line, "^#" .. ADDON_MACRO_COMMAND .. " (" .. unitN .. "[1-9][0-9]*)$", "%1")
 
     if (n == 1) then
         return 0, s
@@ -36,7 +93,7 @@ local function ProcessLine(line)
     end
 end
 
-local function ProcessMacro(macroSlot, targetIndex, partyIndex)
+local function ProcessMacro(macroSlot, targetIndex, partyIndex, unitN)
     local name, icon, body = GetMacroInfo(macroSlot)
 
     if (body == nil) then
@@ -47,19 +104,19 @@ local function ProcessMacro(macroSlot, targetIndex, partyIndex)
 
     for index = 1, #body do
         local c = body:byte(index)
-        local target = "@party" .. tostring(targetIndex)
-        local party = "party" .. tostring(partyIndex)
+        local target = "@" .. unitN .. tostring(targetIndex)
+        local party = unitN .. tostring(partyIndex)
 
         if (c == 10) then
             e = index - 1
-            local code, command = ProcessLine(string.sub(body, s, e))
+            local code, command = ProcessLine(string.sub(body, s, e), unitN)
 
             if (code == 2) then
                 return
             end
 
             if (code == 0 and command == party) then
-                body = string.gsub(body, "@party[1-9][0-9]*", "@party" .. targetIndex)
+                body = string.gsub(body, "@" .. unitN .. "[1-9][0-9]*", "@" .. unitN .. targetIndex)
                 EditMacro(macroSlot, name, icon, body)
             end
 
@@ -68,46 +125,78 @@ local function ProcessMacro(macroSlot, targetIndex, partyIndex)
     end
 end
 
-local function SwapPartyConditionals(targetIndex, partyIndex)
+local function SwapUnitNConditionals(targetIndex, partyIndex, unitN)
     local macroCount = 120 + 18
 
     for i = 1, macroCount, 1 do
-        ProcessMacro(i, targetIndex, partyIndex)
+        ProcessMacro(i, targetIndex, partyIndex, unitN)
     end
 
-    print("\124cff00FF00Replaced #" ..
-        ADDON_NAME .. " party" .. partyIndex .. " macro conditions @partyX with @party" .. targetIndex .. "\124r")
+    local unitName, _ = UnitName(unitN .. targetIndex)
+
+    if (unitName == nil) then
+        unitName = "Unknown"
+    end
+
+    print("|cff00FF00Replaced #" ..
+        ADDON_NAME ..
+        " " ..
+        unitN ..
+        partyIndex ..
+        " macro conditions @" ..
+        unitN .. " with @" .. unitN .. targetIndex .. ". @" .. unitN .. targetIndex .. " is \"" .. unitName .. "\"|r")
 end
 
-SlashCmdList["ARENA_ORDER"] = function(msg, editbox)
-    if (msg == "") then
-        print("\124cffFFFF00Available commands:\n\n\124rparty[1-" ..
-        tostring(PARTY_SIZE - 1) ..
-        "]\124cffFFFF00 - Replaces the @partyX macro conditionals with your party target. Prefix your macros with \"#" ..
-        ADDON_NAME ..
-        " partyX\" where X is the party index between 1 and 4.\n\n\124rreset\124cffFFFF00 - Resets all @partyX macro conditionals to their corresponding #" ..
-        ADDON_NAME .. " partyX macro command.\124r")
-        return
+local function cmd_Reset(msg, editbox)
+    for i = 1, PARTY_SIZE - 1, 1 do
+        -- SwapUnitNConditionals(i, i, "party")
+        AO["party"][i] = i
     end
 
-    if (string.match(msg, "reset")) then
-        for i = 1, PARTY_SIZE - 1, 1 do
-            SwapPartyConditionals(i, i)
+    for i = 1, PARTY_SIZE, 1 do
+        -- SwapUnitNConditionals(i, i, "arena")
+        AO["arena"][i] = i
+    end
+
+    -- print("|cff00FF00All @partyX and @arenaX conditions have been reset.|r")
+end
+
+local function SwapIndicesFunc(a, b, unitN, func)
+    table.exchange(AO[unitN], a, b)
+end
+
+local function RotateIndicesFunc(a, b, unitN, func)
+    local target = a
+    a = table.indexOf(AO[unitN], a)
+
+    if (a == b) then
+        -- do nothing
+    elseif (a > b) then
+        local min = b;
+        b = a - 1
+
+        while (b >= min) do
+            table.exchange(AO[unitN], a, b)
+            a = a - 1
+            b = b - 1
         end
+    elseif (a < b) then
+        local max = b;
+        b = a + 1
 
-        print("\124cff00FF00All @partyX conditions have been reset.\124r")
-        return
+        while (b <= max) do
+            table.exchange(AO[unitN], a, b)
+            a = a + 1
+            b = b + 1
+        end
     end
+end
 
-    if (string.match(msg, "^party[1-9][0-9]*$") == nil) then
-        print("\124cffFF0000\"" .. msg .. "\" is an invalid unit.\124r")
-        return
-    end
-
+local function cmd_Party(msg, editbox)
     local targetName, _ = UnitName("target")
 
     if (targetName == UnitName("player")) then
-        print("\124cffFF0000You cannot target yourself.\124r")
+        print("|cffFF0000You cannot target yourself.|r")
         return
     end
 
@@ -115,15 +204,24 @@ SlashCmdList["ARENA_ORDER"] = function(msg, editbox)
         targetName = DEBUG_UNIT_NAME
     end
 
-    local partyIndex = tonumber(string.gsub(msg, "^party([1-9][0-9]*)$", "%1"), 10)
+    local partyIndex = tonumber(string.gsub(msg, "^p([1-9][0-9]*)$", "%1"), 10)
+
+    if (partyIndex == nil) then
+        partyIndex = tonumber(string.gsub(msg, "^party([1-9][0-9]*)$", "%1"), 10)
+    end
+
+    if (partyIndex == nil) then
+        print("|cffFF0000Party index was nil. Please contact the developer.|r")
+        return
+    end
 
     if (partyIndex >= PARTY_SIZE) then
-        print("\124cffFF0000Party unit index " .. tostring(partyIndex) .. " is outside of range 1-4.\124r")
+        print("|cffFF0000Party unit index " .. tostring(partyIndex) .. " is outside of range 1-4.|r")
         return
     end
 
     if (targetName == nil) then
-        print("\124cffFF0000No target selected.\124r")
+        print("|cffFF0000No target selected.|r")
         return
     end
 
@@ -143,7 +241,7 @@ SlashCmdList["ARENA_ORDER"] = function(msg, editbox)
     end
 
     if (table.getn(partyNames) <= 0) then
-        print("\124cffFF0000You are not in a party.\124r")
+        print("|cffFF0000You are not in a party.|r")
         return
     end
 
@@ -156,31 +254,90 @@ SlashCmdList["ARENA_ORDER"] = function(msg, editbox)
     end
 
     if (targetIndex == nil) then
-        print("\124cffFF0000Target is not in your party.\124r")
+        print("|cffFF0000Target is not in your party.|r")
         return
     end
 
-    SwapPartyConditionals(targetIndex, partyIndex)
-
-    if (targetIndex > partyIndex) then
-        local min = partyIndex
-        partyIndex = targetIndex
-        targetIndex = partyIndex - 1
-
-        while (targetIndex >= min) do
-            SwapPartyConditionals(targetIndex, partyIndex)
-            targetIndex = targetIndex - 1
-            partyIndex = partyIndex - 1
-        end
-    elseif (targetIndex < partyIndex) then
-        local max = partyIndex
-        partyIndex = targetIndex
-        targetIndex = partyIndex + 1
-
-        while (targetIndex <= max) do
-            SwapPartyConditionals(targetIndex, partyIndex)
-            targetIndex = targetIndex + 1
-            partyIndex = partyIndex + 1
-        end
-    end
+    RotateIndicesFunc(targetIndex, partyIndex, "party", SwapUnitNConditionals)
 end
+
+local function cmd_Arena(msg, editbox)
+    local arenaIndex = tonumber(string.gsub(msg, "^a([1-9][0-9]*) [1-9][0-9]*$", "%1"), 10)
+
+    if (arenaIndex == nil) then
+        arenaIndex = tonumber(string.gsub(msg, "^arena([1-9][0-9]*) [1-9][0-9]*$", "%1"), 10)
+    end
+
+    if (arenaIndex == nil) then
+        print("|cffFF0000Arena index was nil. Please contact the developer.|r")
+        return
+    end
+
+    if (arenaIndex > PARTY_SIZE) then
+        print("|cffFF0000Arena unit index " .. tostring(arenaIndex) .. " is outside of range 1-4.|r")
+        return
+    end
+
+    local targetIndex = tonumber(string.gsub(msg, "^a[1-9][0-9]* ([1-9][0-9]*)$", "%1"), 10)
+
+    if (targetIndex == nil) then
+        targetIndex = tonumber(string.gsub(msg, "^arena[1-9][0-9]* ([1-9][0-9]*)$", "%1"), 10)
+    end
+
+    if (targetIndex == nil) then
+        print("|cffFF0000Arena target index was nil. Please contact the developer.|r")
+        return
+    end
+
+    if (targetIndex > PARTY_SIZE) then
+        print("|cffFF0000Arena unit index " .. tostring(targetIndex) .. " is outside of range 1-4.|r")
+        return
+    end
+
+    RotateIndicesFunc(targetIndex, arenaIndex, "arena", SwapUnitNConditionals)
+end
+local function cmd(msg, editbox)
+    if (msg == "") then
+        print("|cffFFFF00Available commands:|n")
+
+        print("|r(p||party)(1-" ..
+            tostring(PARTY_SIZE - 1) ..
+            ")|cffFFFF00 - Replaces the @partyX macro conditionals with your party target. Prefix your macros with \"#" ..
+            ADDON_NAME ..
+            " partyX\" where X is the party index between 1 and " ..
+            tostring(PARTY_SIZE - 1) ..
+            ".|n|n")
+
+        print("|r(r||reset)|cffFFFF00 - Resets all @partyX macro conditionals to their corresponding #" ..
+            ADDON_NAME ..
+            " partyX macro command.|r|n|n")
+
+        print("|r(a||arena)[1-" ..
+            tostring(PARTY_SIZE) ..
+            "] (1-" ..
+            tostring(PARTY_SIZE) ..
+            ")|cffFFFF00 - Replaces the @arenaX macro conditionals with the second parameter arena unit index. Prefix your macros with \"#" ..
+            ADDON_NAME ..
+            " arenaX\" where X is the arena index between 1 and " .. tostring(PARTY_SIZE) .. ".|r|n|n")
+        return
+    end
+
+    if (string.match(msg, "^r$") or string.match(msg, "^reset$")) then
+        cmd_Reset(msg, editbox)
+        return
+    end
+
+    if (string.match(msg, "^p[1-9][0-9]*$") or string.match(msg, "^party[1-9][0-9]*$")) then
+        cmd_Party(msg, editbox)
+        return
+    end
+
+    if (string.match(msg, "^a[1-9][0-9]* [1-9][0-9]*$") or string.match(msg, "^arena[1-9][0-9]* [1-9][0-9]*$")) then
+        cmd_Arena(msg, editbox)
+        return
+    end
+
+    print("|cffFF0000Invalid command. Use /ao for help.|r")
+end
+
+SlashCmdList["ARENA_ORDER"] = cmd
